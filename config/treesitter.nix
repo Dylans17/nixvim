5cccc8e82328d8fef94ff7fff83d7afecd01caca
Dylan Strickley <dylans2499@gmail.com>
2024-07-14 01:48:16 -0400

    Kickstart setup, based on JMartJonesy/kickstart.nixvim

---
+++ config/treesitter.nix
@@ -0,0 +1,39 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  # Highlight, edit, and navigate code
+  # https://nix-community.github.io/nixvim/plugins/treesitter/index.html
+  plugins.treesitter = {
+    enable = true;
+    
+    settings = {
+      ensureInstalled = [
+        "bash"
+          "c"
+          "diff"
+          "html"
+          "lua"
+          "luadoc"
+          "markdown"
+          "vim"
+          "vimdoc"
+      ];
+
+      highlight = {
+        additional_vim_regex_highlighting = true;
+        enable = true;
+      };
+
+      indent = {
+        enable = true;
+      };
+    };
+
+    # There are additional nvim-treesitter modules that you can use to interact
+    # with nvim-treesitter. You should go explore a few and see what interests you:
+    #
+    #    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    #    - Show your current context: https://nix-community.github.io/nixvim/plugins/treesitter-context/index.html
+    #    - Treesitter + textobjects: https://nix-community.github.io/nixvim/plugins/treesitter-textobjects/index.html
+  };
+}
